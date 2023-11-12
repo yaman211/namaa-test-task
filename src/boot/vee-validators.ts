7bbdb66b3c085c9753117a07be373b9b64607a3e
Yaman <yamanwaza@gmail.com>
2023-11-12 19:18:46 +0300

    add some vee global validators

---
+++ src/boot/vee-validators.ts
@@ -0,0 +1,38 @@
+import { boot } from 'quasar/wrappers';
+import { defineRule } from 'vee-validate';
+
+defineRule('required', (value: any) => {
+   if (!value || !value.length) {
+      return 'This field is required';
+   }
+   return true;
+});
+
+defineRule('minLength', (value: string, [limit]: [number]): true | string => {
+   // The field is empty so it should pass
+   if (!value || !value.length) {
+      return true;
+   }
+   if (value.length < limit) {
+      return `This field must be at least ${limit} characters`;
+   }
+   return true;
+});
+
+defineRule('minMax', (value: number, [min, max]: [number, number]) => {
+   if (!value) {
+      return true;
+   }
+   const numericValue = Number(value);
+   if (numericValue < min) {
+      return `This field must be greater than ${min}`;
+   }
+   if (numericValue > max) {
+      return `This field must be less than ${max}`;
+   }
+   return true;
+});
+
+export default boot(() => {
+   return;
+});
