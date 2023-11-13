ff9b23625ae0fb6021d17a06a9493f41384c42cb
Yaman <yamanwaza@gmail.com>
2023-11-13 10:41:13 +0300

    getUUID function and using it in actor model

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -7,3 +7,12 @@ export const dateTimeFormatter = (date: Date) => {
       dateStyle: 'short',
    }).format(typeof date == 'string' ? new Date(date) : date);
 };
+
+export const getUUID = () => {
+   return '10000000-1000-4000-8000-100000000000'.replace(/[018]/g, (c: any) =>
+      (
+         c ^
+         (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+      ).toString(16)
+   );
+};
